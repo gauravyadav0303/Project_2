b7599df80ceb8034853e51156d6a15ff3e3fba2c
gauravyadav0303 <96978698+gauravyadav0303@users.noreply.github.com>
2023-08-19 21:37:03 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        ubuntu: ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
+
